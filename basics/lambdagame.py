4473baac31aad7d8f2fa1a466aab74b83b14ed3b
rezaprogrammer <sherafat.us@gmail.com>
2014-04-11 21:43:05 -0700

    Some lambda games.

--- basics/lambdagame.py
+++ basics/lambdagame.py
@@ -9,7 +9,18 @@ def test_lambda():
     for i in range(10):
         print(f(2)(i))
 
+def test_map():
+    def f(n): return lambda x: n**x
+    l = list(range(10))
+    for i in range(10):
+        print(map(f(i), l))
 
+def test_sets():
+    s = {3,1,2,1}
+    print(s)
+    
 if __name__ == '__main__':
-    test_lambda()
+    #test_lambda()
+    #test_map()
+    test_sets()
     pass
